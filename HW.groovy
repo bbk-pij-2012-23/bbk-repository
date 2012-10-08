9a6510087bcd340bd923ab776f3880fcd9b3b62f
bbk-pij-2012-23 <eleanor.mann@yahoo.co.uk>
2012-10-08 20:52:50 +0200

    Update HW.groovy
    added multiplication

--- HW.groovy
+++ HW.groovy
@@ -1,3 +1,12 @@
 println "hello world!"
 println "is that really all you want me to do?"
-println "what about some sort of calculation?"
+println "what about some sort of calculation? How about I multiply some numbers for you?"
+print "Enter a number: "
+String str = System.console().readLine()
+double num1 = Double.parseDouble(str)
+print "Enter another number: "
+String str1 = system.console().readLine()
+double num2 = Double.parseDouble(str1)
+double answer = num1*num2
+println num1 + " multiplied by " + num2 + " is " + answer
+
