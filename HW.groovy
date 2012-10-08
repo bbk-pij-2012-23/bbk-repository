6dc9055087574a086e236baf6eed204f1fc9f0fa
unknown <eleanor.mann@bbk.com>
2012-10-08 20:34:52 +0100

    added the request line

--- HW.groovy
+++ HW.groovy
@@ -5,8 +5,10 @@ print "Enter a number: "
 String str = System.console().readLine()
 double num1 = Double.parseDouble(str)
 print "Enter another number: "
-String str1 = system.console().readLine()
+String str1 = System.console().readLine()
 double num2 = Double.parseDouble(str1)
 double answer = num1*num2
 println num1 + " multiplied by " + num2 + " is " + answer
-
+println "how about a game of poker?"
+print "take 5 cards and key in their numbers and suits: "
+ 
