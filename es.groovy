9c8c5c7b67340336a6af3e054bad55bcd7de89a2
unknown <eleanor.mann@bbk.com>
2012-10-15 20:30:09 +0100

    added second part

--- es.groovy
+++ es.groovy
@@ -1,14 +1,33 @@
-print "enter some text: "
-String str = System.console().readLine()
+print "enter some text: ";
+String str = System.console().readLine();
 
 int i = 0;
 int es = 0;
 while (i < str.length()) {
 	String s = str.charAt(i);
-	i++
+	i++;
 	if (s=="e") {
-		es++
+		es++;
 	}
 }	
 println "You have typed " + es + " Es";
 	
+
+	//next section
+
+print "now enter some more text: ";
+str = System.console().readLine();
+println "What letter shall I count? "
+String letter = System.console().readLine();
+i = 0;
+es = 0;
+
+while (i < str.length()) {
+	s = str.charAt(i);
+	i++;
+	if (s == letter) {
+		es++;
+	}	
+}	
+
+println "You have typed " + es + " " + letter + "s"
