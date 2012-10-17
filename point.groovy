4ece0016ba289fd721602eacd11a73644540e6f7
unknown <eleanor.mann@bbk.com>
2012-10-17 12:05:40 +0100

    completed without methods

--- point.groovy
+++ point.groovy
@@ -30,12 +30,29 @@ thirdpoint.y = Double.parseDouble(str);
 
 println "your coordinates are 1(" + firstpoint.x + "," + firstpoint.y + "), 2(" + secpoint.x + "," +secpoint.y + "), 3(" + thirdpoint.x + "," + thirdpoint.y + ")";
 
-double distance(double x1, double y1, double x2, double y2) {
-	double result1 = ((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2))/((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2));
-	return result1;
-	
+// 1v2; 1v3; 2v3 
+
+double d1 = ((firstpoint.x - secpoint.x) * (firstpoint.x - secpoint.x)) + ((firstpoint.y - secpoint.y) * (firstpoint.y-secpoint.y))
+double result1 = Math.sqrt(d1)
+
+double d2 = ((firstpoint.x - thirdpoint.x) * (firstpoint.x - thirdpoint.x)) + ((firstpoint.y - thirdpoint.y) * (firstpoint.y-thirdpoint.y)) ;
+double result2 = Math.sqrt(d2);
+
+double d3 = ((secpoint.x - thirdpoint.x) * (secpoint.x - thirdpoint.x)) + ((secpoint.y - thirdpoint.y) * (secpoint.y-thirdpoint.y));
+double result3 = Math.sqrt(d3);
+
+println "distance between first and second point = " + result1;
+println "distance between first and third point = " + result2;
+println "distance between second and third point = " + result3;
+
+if (result1 < result2 && result1 < result3) {
+	print "the distance between the first and second point is the shortest";
+}
+else if (result2 < result1 && result2 < result3) {
+	print "the distance between the first and third point is the shortest";
+}
+else {
+	print "the distance between the second and third point is the shortest";
 }	
-	
-	print result1; 
 
 
