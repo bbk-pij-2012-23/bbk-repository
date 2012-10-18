b4b26e7940068257b630aef8e58c36b9f960754c
unknown <eleanor.mann@bbk.com>
2012-10-18 13:02:55 +0100

    D3 11 Rectangle

--- rect.groovy
+++ rect.groovy
@@ -1,9 +1,18 @@
 // D3 11 rectangle
+
+class Point {
+	double x;
+	double y;
+}
+
+Point upleft = new Point();
+Point downright = new Point();
+
 class Rectangle {
 	Point upleft;
 	Point downright;
-
 }
+
 
 println "please enter the coordinates of the top left and bottom right corners of your rectangle. Use the format n,n (number comma number, without spaces).";
 print "top left = ";
@@ -23,6 +32,31 @@ while (i < j) {
 	i++
 }
 
-println upleft.x + "," + upleft.y;
+print "bottom right = ";
+str = System.console().readLine();
+
+i = 0;
+j = str.length()
+
+while (i < j) {
+	c = str.charAt(i);
+	if (c == ',') {
+		downright.x = Double.parseDouble(str.substring(0, i));
+		downright.y = Double.parseDouble(str.substring(i + 1, j));
+	
+	}	
+	i++
+}
+
+if (downright.x == 0 || downright.y == 0 || upleft.x == 0 || upleft.y == 0) {
+print "this won't work at the moment, I need to change the calculation from multiplication to addition";
+}
+else {
+	d = (upleft.x - downright.x) * (upleft.y - downright.y);
+
+	println upleft.x + "," + upleft.y;
+	println downright.x + "," + downright.y;
+	println "the area is " + area;
+}
 
 	
