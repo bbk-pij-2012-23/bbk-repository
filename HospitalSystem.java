398aa54486fb97d0b6fcf78fce1dd0cc0bb73c76
unknown <eleanor.mann@bbk.com>
2012-10-24 18:54:15 +0100

    deleter corrected and exercise completed

--- HospitalSystem.java
+++ HospitalSystem.java
@@ -12,102 +12,133 @@
 					pos = pos.nextPatient;
 				}
 				pos.nextPatient = newPatient; //this is where the link to next patient is
-				newPatient.prevPatient = pos;
+				newPatient.prevPatient = pos; //this is the link to the previous patient
 			}
 			
 		}	
 		
-		public Patient deletePatient(Patient oldPatient, Patient start) { //need to delete the prevPatient reference too
 		
-			if (start == null) {
-				return null;
-			}
-			
-			if (start.name.equals(oldPatient.name)){
-				return start.nextPatient;
-			}
-			else { 
-				start.nextPatient = deletePatient(oldPatient, start.nextPatient);
-				return start;
+		public void deletePatient(Patient oldPatient) { 
+			Patient current = patientListStart;
+			while (current != null) {
+				if (current.name.equals(oldPatient.name)){
+					if (current.nextPatient != null) {
+						current.nextPatient.prevPatient = current.prevPatient;
+					}
+					else {
+						current.prevPatient.nextPatient = null;
+						
+					}
+					if (current.prevPatient != null) {
+						current.prevPatient.nextPatient = current.nextPatient;
+					}
+					else {
+						current.nextPatient.prevPatient = null;
+						patientListStart = current.nextPatient; //else the head is still pointing to firstperson
+					}
+					
+					break;
+				}
+				else {
+					current = current.nextPatient;
+					
+				}	
 			}
 		}	
 		
-			public void peep(){
-			
+		
+		public void peep(){
 			
 			Patient counter = patientListStart;
-			while ((counter != null) && (!counter.name.equals("-1"))) {
+			while (counter != null) { //&& (!counter.name.equals("-1"))) {
 				System.out.println("Patient name: " + counter.name);
-			/*if (counter.prevPatient != null){
-					System.out.println("Previous patient: " + counter.prevPatient.name);
+					
+				if (counter.nextPatient != null) {
+					counter = counter.nextPatient;
 				}
-				if (counter.nextPatient != null){
-					System.out.println("Next Patient: " + counter.nextPatient.name);
-				}*/
+				else {
+					break;
+				}
+			}	
 			
-				counter = counter.nextPatient;
-			}
-			if (counter.name.equals("-1")){
-				while (counter.prevPatient != null) {
+			while (counter != null) {
+				System.out.println("Patient name: " + counter.name);
+				
+				if (counter.prevPatient != null) {
 					counter = counter.prevPatient;
-					System.out.println("Patient name: " + counter.name);
-					/*if (counter.prevPatient != null){
-						System.out.println("Previous patient: " + counter.prevPatient.name);
-					}
-					if (counter.nextPatient != null){
-						System.out.println("Next Patient: " + counter.nextPatient.name);
-					}
-					*/
 				}
+				else {
+					break;
+				}	
 			}
-		}		
+		}	
+
+				
 		public static void main(String[] args) {
+			boolean stop = false;
 			HospitalSystem hospitalSystem = new HospitalSystem();
 			hospitalSystem.launch();
 			
 			hospitalSystem.peep();
 			hospitalSystem.deleterlaunch();
+			hospitalSystem.peep();
+			
+			//hospitalSystem.addMore();
 			//
+			//hospitalSystem.peep();
+			System.out.println("what would you like to do next? Choose from the following options:");
+			System.out.println("0 - Exit");
+			System.out.println("1 - Add patients");
+			System.out.println("2 - Delete a patient");
+			System.out.println("3 - Display patient list");
+			String str = System.console().readLine();
+			if (str.equals("0")) {
+					
+			}
+			if (str.equals("1")) {
+				hospitalSystem.launch();
+			}
+			if(str.equals("2")) {
+				hospitalSystem.deleterlaunch();
+			}
+			if(str.equals("3")) {
+				hospitalSystem.peep();
+			}		
+			
 			
 		}
 	
+			
+			
+			
 		public void launch() {
 			// add someting to take in new patients from user
-			System.out.println("key in patient names, hitting enter after each. enter -1 when you have finished entering names");
-			System.out.println("Patient: ");
-			String str = System.console().readLine();
-			Patient firstPatient = new Patient(str);
-			addPatient(firstPatient);
-			do{
+			
+				System.out.println("key in patient names, hitting enter after each. enter -1 when you have finished entering names");
+			while(true) {	
 				System.out.println("Patient: ");
-				str = System.console().readLine();
-				Patient anotherPatient = new Patient(str);
-				addPatient(anotherPatient);		
-			} while (!str.equals("-1"));
-		}	
+				String str = System.console().readLine();
+				if (str.equals("-1")){
+					break;
+				}
+				
+				Patient firstPatient = new Patient(str);
+				addPatient(firstPatient);
+			 
+				}	
+			} 
+			
 		
 		public void deleterlaunch() {
  				
 			System.out.println("What is the name of the patient so I can remove them from the records?");
 			String str = System.console().readLine();
 			Patient removePatient = new Patient(str);
-			patientListStart = deletePatient(removePatient, patientListStart);	
-			
-			
-			Patient counter = patientListStart;
-			while ((counter != null) && (!counter.name.equals("-1"))) {
-				System.out.println("Patient name: " + counter.name);
-				counter = counter.nextPatient;
-			}
-			if (counter.name.equals("-1")){
-				while (counter.prevPatient != null) {
-					counter = counter.prevPatient;
-					System.out.println("Patient name: " + counter.name);
-				}
-			}
-			
+			deletePatient(removePatient);	
 		
-			
 		}	
+		
+	
+		
 		
 	}
